637eb27e5cdcae2e000e6ed7a51e471c53de2104
Wesly Backers <45541801+WeslyBackers@users.noreply.github.com>
2026-07-14 15:11:26 +0200

    titles adjusted

--- insert_new_user.sql
+++ insert_new_user.sql
@@ -2,7 +2,7 @@
 INSERT INTO users (email, password_hash, first_name, last_name, default_production_line_id, is_active)
 VALUES (
   'wesly.backers@mow.vlaanderen.be',
-  '$2b$10$hrBa15K9pAJGYRcJ/BvbfuE2BwvQV9VjKGIWcoaOskhqyK1/KPyXK', -- password: admin123
+  '$2b$10$hrBa15K9pAJGYRcJ/BvbfuE2BwvQV9VjKGIWcoaOskhqyK1/KPyXK', -- password shs
   'Wesly',
   'Backers',
   2,     -- default production line id (1=ZK, 2=IENC, 3=PILOT_ENC, 4=PUBL)
@@ -29,7 +29,7 @@ VALUES (
   true   -- can_publish
 );
 
--- 3. Grant rights per production line (repeat for each line the user needs)
+-- 4. Grant rights per production line (repeat for each line the user needs)
 INSERT INTO user_production_line_rights (user_id, production_line_id, can_view, can_edit, can_publish)
 VALUES (
   (SELECT id FROM users WHERE email = 'wesly.backers@mow.vlaanderen.be'),
